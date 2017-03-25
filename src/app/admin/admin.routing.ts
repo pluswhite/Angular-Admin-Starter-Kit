822ec0268180ce3eabd300a3e0ea3302e18a753e
Eric Zhang <beefjava@gmail.com>
2017-03-25 23:57:49 +0800

    add top menu

--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     path: 'admin',
     component: AdminComponent,
     canActivate: [
-      Angular2TokenService
+      // Angular2TokenService
     ],
     children: [
       {
