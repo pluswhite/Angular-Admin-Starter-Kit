5b741fde002b38d550f2cfc130e70194527370a1
EricThsi <beefjava@gmail.com>
2020-02-10 22:20:28 +0800

    feat(errorhandler): add error handler

--- src/app/models/rest.datasource.ts
+++ src/app/models/rest.datasource.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -26,8 +29,25 @@ export class RestDataSource {
     };
   }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`,
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.baseUrl}posts`);
+    return this.http
+      .get<Post[]>(`${this.baseUrl}posts`)
+      .pipe(catchError(this.handleError));
   }
 
   savePost(post: Post): Observable<Post> {
