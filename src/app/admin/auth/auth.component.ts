8e2a8faee0c76d01277f7c5bef05352e616c386e
EricThsi <beefjava@gmail.com>
2020-02-19 21:08:53 +0800

    feat(component): add auth component

---
+++ src/app/admin/auth/auth.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
+})
+export class AuthComponent implements OnInit {
+  public email: string;
+  public password: string;
+  public errorMessage: string;
+
+  constructor(private router: Router, private auth: AuthService) {}
+
+  ngOnInit(): void {}
+
+  authenticate(form: NgForm) {
+    if (form.valid) {
+      this.auth.authenticate(this.email, this.password).subscribe(res => {
+        if (res) {
+          this.router.navigateByUrl('/admin/main');
+        }
+        this.errorMessage = 'Authentication Failed!';
+      });
+    } else {
+      this.errorMessage = 'Form Data Invalid!';
+    }
+  }
+}
